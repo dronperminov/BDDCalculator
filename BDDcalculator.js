7e4254b337069eaee4023fd16ac3b7d8ef4e6a3d
dronperminov <dronperminov@gmail.com>
2021-04-10 11:38:26 +0300

    add moving of nodes

--- BDDcalculator.js
+++ BDDcalculator.js
@@ -9,6 +9,16 @@ function BDDCalculator(inputBox, variablesBox, resultBox, canvas, width, height)
     this.canvas.width = width
     this.canvas.height = height
     this.ctx = this.canvas.getContext('2d')
+
+    let calculator = this
+    this.canvas.addEventListener('mousedown', function(e) { calculator.MouseDown(e) })
+    this.canvas.addEventListener('mouseup', function(e) { calculator.MouseUp(e) })
+    this.canvas.addEventListener('mousemove', function(e) { calculator.MouseMove(e) })
+
+    this.isPressed = false
+    this.prevX = 0
+    this.prevY = 0
+    this.activeNode = null
 }
 
 // построение BDD по выражению
@@ -16,46 +26,38 @@ BDDCalculator.prototype.Solve = function() {
     console.clear()
     try {
         this.ctx.clearRect(0, 0, this.width, this.height)
+        this.funcTable = new FunctionTable(this.inputBox.value)
+        this.variablesNames = this.variablesBox.value.split(/ +/g)
 
-        let funcTable = new FunctionTable(this.inputBox.value)
-        let variablesNames = this.variablesBox.value.split(/ +/g)
-
-        this.ctx.fillStyle = '#000'
-        this.ctx.textAlign = 'left'
-        this.ctx.textBaseline = 'middle'
-        this.ctx.font = '18px Consolas'
-
-        this.ctx.fillText("Формула: " + funcTable.calculator.expression, 10, 18)
-        this.ctx.fillText("Порядок переменных: " + variablesNames.join(", "), 10, 40)
-
-        if (!funcTable.HaveAllVariables(variablesNames))
+        if (!this.funcTable.HaveAllVariables(this.variablesNames))
             throw "Variable names not match with variables in expression"
 
-        this.resultBox.innerHTML = "<p><b>Введённое выражение:</b> " + funcTable.calculator.expression + "</p>"
-        this.resultBox.innerHTML += "<p><b>Распаршенное выражение:</b> " + funcTable.calculator.ToString() + "</p>"
-        this.resultBox.innerHTML += "<p><b>Вектор функции:</b> " + funcTable.vector.join("") + "</p>"
+        this.resultBox.innerHTML = "<p><b>Введённое выражение:</b> " + this.funcTable.calculator.expression + "</p>"
+        this.resultBox.innerHTML += "<p><b>Распаршенное выражение:</b> " + this.funcTable.calculator.ToString() + "</p>"
+        this.resultBox.innerHTML += "<p><b>Вектор функции:</b> " + this.funcTable.vector.join("") + "</p>"
         this.resultBox.innerHTML += "<p><b>Таблица истинности:</b></p>"
-        this.resultBox.appendChild(funcTable.ToHTML())
+        this.resultBox.appendChild(this.funcTable.ToHTML())
 
-        let variables = Object.keys(funcTable.calculator.variables)
+        let variables = Object.keys(this.funcTable.calculator.variables)
 
         for (let i = 0; i < variables.length; i++) {
             this.resultBox.innerHTML += "<p><b>Разбиение по переменной " + variables[i] + ":</b><br>"
-            let splited = funcTable.SplitByVariable(variables[i])
+            let splited = this.funcTable.SplitByVariable(variables[i])
             this.resultBox.innerHTML += "TRUE: " + splited.trueExpression + "<br>"
             this.resultBox.innerHTML += "FALSE: " + splited.falseExpression
             this.resultBox.innerHTML += "</p>"
         }
 
-        let robdd = funcTable.GetROBDD(variablesNames)
+        let robdd = this.funcTable.GetROBDD(this.variablesNames)
 
-        this.DrawBDD(robdd.robdd, robdd.applyes)
+        this.robdd = robdd.robdd
+        this.applyes = robdd.applyes
+        this.DrawBDD()
 
         this.resultBox.innerHTML += "<p><b>Построение ROBDD:</b><br>"
         this.resultBox.innerHTML += robdd.solve.join("<br>")
         this.resultBox.innerHTML += "</p>"
 
-        console.log(robdd.robdd, this.width)
     }
     catch (error) {
         this.resultBox.innerHTML = "<p><b>Ошибка:</b> " + error + "</p>"
@@ -90,13 +92,12 @@ BDDCalculator.prototype.DrawEdges = function(node) {
     this.DrawEdges(node.low)
     this.DrawEdges(node.high)
 
-    console.log(node.value, node.high.value, node.low.value, node.x, node.y, node.high.x, node.high.y, node.low.x, node.low.y)
     this.DrawLine(node.x, node.y, node.high.x, node.high.y, 0)
     this.DrawLine(node.x, node.y, node.low.x, node.low.y, 1)
 }
 
 // задание координат узлов
-BDDCalculator.prototype.SetNodeCoordinates = function(node, maxLevel, y0) {
+BDDCalculator.prototype.SetNodeCoordinates = function(node, maxLevel, y0, radius) {
     let ix = (node.index + 1.5) / (1 << node.level) - 1;
     let iy = (node.level + 0.5) / (maxLevel + 1);
 
@@ -109,19 +110,20 @@ BDDCalculator.prototype.SetNodeCoordinates = function(node, maxLevel, y0) {
 
     node.x = x
     node.y = y
+    node.radius = radius
 }
 
 // отрисока узла
-BDDCalculator.prototype.DrawNode = function(node, maxLevel, radius) {
+BDDCalculator.prototype.DrawNode = function(node) {
     this.ctx.fillStyle = '#efeeff'
     this.ctx.strokeStyle = '#000'
     this.ctx.beginPath()
 
     if (this.IsLeafNode(node)) {
-        this.ctx.rect(node.x - radius, node.y - radius, 2 * radius, 2 * radius)
+        this.ctx.rect(node.x - node.radius, node.y - node.radius, 2 * node.radius, 2 * node.radius)
     }
     else {
-        this.ctx.arc(node.x, node.y, radius, 0, Math.PI * 2)
+        this.ctx.arc(node.x, node.y, node.radius, 0, Math.PI * 2)
     }
 
     this.ctx.fill()
@@ -148,27 +150,94 @@ BDDCalculator.prototype.IsLeafNode = function(node) {
 }
 
 // отрисовка диаграммы
-BDDCalculator.prototype.DrawBDD = function(robdd, applyes) {
+BDDCalculator.prototype.DrawBDD = function() {
     let maxLevel = 0
 
-    for (let node of Object.values(applyes))
+    for (let node of Object.values(this.applyes))
         if (node.level > maxLevel && !this.IsLeafNode(node))
             maxLevel = node.level
 
     maxLevel += 1
 
-    for (let node of Object.values(applyes))
+    for (let node of Object.values(this.applyes))
         if (this.IsLeafNode(node))
             node.level = maxLevel
 
     let y0 = 5
     let radius = 20
 
-    for (let node of Object.values(applyes))
-        this.SetNodeCoordinates(node, maxLevel, y0)
+    for (let node of Object.values(this.applyes))
+        this.SetNodeCoordinates(node, maxLevel, y0, radius)
 
-    this.DrawEdges(robdd)
+    this.Draw()
+}
 
-    for (let node of Object.values(applyes))
-        this.DrawNode(node, maxLevel, radius)
+// отрисовка дерева на картинке
+BDDCalculator.prototype.Draw = function() {
+    this.ctx.clearRect(0, 0, this.width, this.height)
+    this.DrawEdges(this.robdd)
+
+    for (let node of Object.values(this.applyes))
+        this.DrawNode(node)
+
+    this.ctx.fillStyle = '#000'
+    this.ctx.textAlign = 'left'
+    this.ctx.textBaseline = 'middle'
+    this.ctx.font = '18px Consolas'
+    this.ctx.fillText("Формула: " + this.funcTable.calculator.expression, 10, 18)
+    this.ctx.fillText("Порядок переменных: " + this.variablesNames.join(", "), 10, 40)
+}
+
+// проверка, что мышь находится над узлом
+BDDCalculator.prototype.IsMouseHoverNode = function(x, y, node) {
+    let dx = x - node.x
+    let dy = y - node.y
+
+    return dx*dx + dy*dy < node.radius * node.radius
+}
+
+BDDCalculator.prototype.GetHoverNode = function(x, y) {
+    for (let node of Object.values(this.applyes))
+        if (this.IsMouseHoverNode(x, y, node))
+            return node
+
+    return null
+}
+
+// обработка нажатия мыши
+BDDCalculator.prototype.MouseDown = function(e) {
+    this.isPressed = true
+    this.activeNode = this.GetHoverNode(e.offsetX, e.offsetY)
+    this.prevX = e.offsetX
+    this.prevY = e.offsetY
+}
+
+// обработка отпускания мыши
+BDDCalculator.prototype.MouseUp = function(e) {
+    this.isPressed = false
+    this.activeNode = null
+}
+
+// обработка перемещения мыши
+BDDCalculator.prototype.MouseMove = function(e) {
+    if (!this.isPressed || this.activeNode == null)
+        return
+
+    let dx = e.offsetX - this.prevX
+    let dy = e.offsetY - this.prevY
+    let delta = 5
+    let node = this.activeNode
+    let radius = node.radius
+
+    if (Math.abs(dx) > delta && node.x + dx >= radius && node.x + dx < this.width - radius) {
+        this.activeNode.x += dx
+        this.prevX = e.offsetX
+    }
+
+    if (node.level > 0 && Math.abs(dy) > delta && node.y + dy >= radius && node.y + dy < this.height - radius) {
+        this.activeNode.y += dy
+        this.prevY = e.offsetY
+    }
+
+    this.Draw()
 }
